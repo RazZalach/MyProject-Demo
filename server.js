6763a04388ce388bf34eb7fe6e4dbb2e4a3df6a5
raz zalach <raz62015@gmail.com>
2022-08-02 18:10:34 +0300

    server change  code

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 const http=require('http');
+const app=require('./app');
+const server=http.createServer(app);
+const port=process.env.PORT || 5656 ;
+server.listen(port,()=>{
+console.log('server on air');
+});
